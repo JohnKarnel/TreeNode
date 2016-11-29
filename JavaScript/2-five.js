a7d3834f7662fd297d45512a6da8375f825d2d44
Ivan Kornilov <john_karnel@mail.ua>
2016-11-29 15:42:48 +0200

    insertAfter fucntion

--- JavaScript/2-five.js
+++ JavaScript/2-five.js
@@ -29,6 +29,27 @@ function Node(parent, data) {
   this.last = null;
 }
 
+Node.prototype.insertAfter = function (node) {
+  if (node) {
+    node.next = null;
+    node.first = null;
+    node.last = null;
+    node.parent = this.parent;
+    node.tree = this.tree;
+    node.tree.count++;
+    node.parent.count++;
+    node.prev = this;
+    if (this.next) {
+      node.next = this.next;
+      this.next.prev = node;
+    }
+    else {
+      this.next = node;
+      node.parent.last = node;
+    }
+  }
+}
+
 Tree.prototype.forEach = function (callback) {
     GoNext(callback, this.root);
 }
